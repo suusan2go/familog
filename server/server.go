f446852b0a95e554a95ba38c0bc7b188732a3d79
suusan2go <ksuzuki180@gmail.com>
2018-05-07 23:22:07 +0900

    get currentAuth from context

--- server/server.go
+++ server/server.go
@@ -1,11 +1,37 @@
 package server
 
 import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+
+	"fmt"
+
 	"github.com/suusan2go/familog"
 	"github.com/suusan2go/familog/app"
+	"github.com/suusan2go/familog/app/domain"
+	"google.golang.org/grpc/metadata"
 )
 
 type FamilogServer struct {
 	familog.FamilogServer
 	Registry *app.Registry
+}
+
+// return domain.Auth from gRPC metadata x-endpoint-api-userinfo
+func (s *FamilogServer) currentAuth(ctx context.Context) (*domain.Auth, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("failed to load metadata")
+	}
+	info := md["x-endpoint-api-userinfo"][0]
+	b, err := base64.StdEncoding.DecodeString(info)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode auth jwt: %v", err)
+	}
+	var auth *domain.Auth
+	if err = json.Unmarshal(b, auth); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal authjson: %v", err)
+	}
+	return auth, nil
 }
